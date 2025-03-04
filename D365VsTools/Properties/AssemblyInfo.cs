14b657cfd30d43e243031fcb9139b5712ff3fe60
Angel A. Rodriguez <angel.rodriguez@outlook.at>
2025-03-04 12:41:17 +0100

    Add Visual Studio Pro & Enteprise as  Installation Target

--- D365VsTools/Properties/AssemblyInfo.cs
+++ D365VsTools/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.22082.1129")]
-[assembly: AssemblyFileVersion("1.0.22082.1129")]
+[assembly: AssemblyVersion("1.0.25063.1139")]
+[assembly: AssemblyFileVersion("1.0.2025.0304")]
